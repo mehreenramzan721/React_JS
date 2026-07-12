47927cdf691d40593d7d1c5c9e0b2a6cac056220
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-12 12:03:40 +0000

    done

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
-
+import Chai from './chai'
 
 function App() {
   return (
-    <h1>Chai aur code</h1>
+    <>
+      <Chai/>
+      <h1>hello</h1>
+    </>
   );
 }
 
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from '/.App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -9,7 +9,4 @@ root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-
 )
-
-export default App;
