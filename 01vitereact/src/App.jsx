4632cd0b9216db43559573d9dab79f4a18052d80
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-12 10:53:15 +0000

    chai

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-
+import chai from '/.chai';
 function App() {
 
   return (
-   <h1>Chai aur react with vite</h1>
+  //  <h1>Chai aur react with vite</h1>
+  <chai/>
   )
 }
 
