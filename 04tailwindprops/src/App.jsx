b52a87cf223ee88d738e0ed201214006ff92924e
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-14 18:51:26 +0000

    updated cards

--- 04tailwindprops/src/App.jsx
+++ 04tailwindprops/src/App.jsx
@@ -4,13 +4,21 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+import Card from './components/Card.jsx'
+
+function App(props) {
   const [count, setCount] = useState(0)
+  console.log('Props : ', props)
+  let myObj = {
+    name: "Mehr",
+    age : 20
+  }
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mg-4'>Tailwind TEST</h1>
-
+      <Card channel = 'Chai aur react' my = {myObj}/>
+      <Card />
       {/* 
           // copy the code from tailwind css website
       <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
@@ -27,22 +35,7 @@ function App() {
         </span>
       </div>
     </div> */}
-    
-
-    <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-        <a href="#">
-            <img className="rounded-base" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR8Lf_XMIGZBtayJpJNPFmmCALIgEqjfv2qj3bedJAQw&s=10" alt="" />
-        </a>
-        <a href="#">
-            <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Streamlining your design process today.</h5>
-        </a>
-        <p className="mb-6 text-body">In today’s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
-        <a href="#" className="inline-flex items-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-            Read more
-            <svg className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
-        </a>
-    </div>
-
+  
     </>
   )
 }
