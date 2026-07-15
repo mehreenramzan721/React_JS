ef163bab1e488a8e82c1e05b89ed450129d24a1b
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-15 08:48:43 +0000

    completed

--- 05bgchanger/src/App.jsx
+++ 05bgchanger/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
     <>
       <div className="w-full h-screen duration-200"
         style={{ backgroundColor: color }}>
-        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="fixed flex flex-wrap justify-center bottom-12 left-1/2 -translate-x-1/2 w-fit rounded-full px-4"
+        style={{ backgroundColor: "white" }}>
           <button
           onClick={() => setColor("red")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
@@ -21,6 +22,22 @@ function App() {
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "blue"}}
           >Blue</button>
+          <button
+          onClick={() => setColor("grey")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "grey"}}
+          >grey</button>
+          <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "yellow"}}
+          >yellow</button>
+          <button
+          onClick={() => setColor("purple")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "purple"}}
+          >purple</button>
+
         </div>
       </div>
     </>
