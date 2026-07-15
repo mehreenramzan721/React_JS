3339a83696b19b1719fb6decdb6c56007e1bdd71
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-15 13:36:02 +0000

    updates

--- 06passwordgenerator/src/App.jsx
+++ 06passwordgenerator/src/App.jsx
@@ -1,45 +1,47 @@
-import { useState, useCallback ,useEffect ,useRef } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
 
 
 function App() {
-  const [length, setLength] = useState(8);
+  const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
+  //useRef hook
   const passwordRef = useRef(null)
 
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-  const passwordGenerator= useCallback(()=>{
-    let pass = "";
-    let str = "ABCDEFGHIJHKLMNOPQRTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(numberAllowed){
-      str += "1234567890";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
     }
-    if(charAllowed){
-      str += "! @ # $ % ^ & * ( ) _ - + = { }  | ; :   , . < >  ? ~ ";
-    }
 
-    for(let i=1; i<= length; i++){
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
-    }
-    setPassword(pass);
-  } , [length, numberAllowed, charAllowed, setPassword])
+    setPassword(pass)
 
-    const copyPasswordToClipboard = useCallback(() => {
-      passwordRef.current?.select();
-      passwordRef.current?.setSelectionRange(0, 999);
-      window.navigator.clipboard.writeText(password)
-    }, [password])
 
-    useEffect(() => {
-      passwordGenerator()
-    }, [length, numberAllowed, charAllowed, passwordGenerator])
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
-    <>
+    
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
             value={password}
@@ -66,7 +68,7 @@ function App() {
           />
           <label>Length: {length}</label>
       </div>
-      <div className="flex items-center gap-x-1 ">
+      <div className="flex items-center gap-x-1">
       <input
           type="checkbox"
           defaultChecked={numberAllowed}
@@ -88,8 +90,9 @@ function App() {
           />
           <label htmlFor="characterInput">Characters</label>
       </div>
-      </div>
-    </>
+    </div>
+</div>
+    
   )
 }
 
