fb1293577f9188b32c5db93feb4eeb10a00c18bc
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-17 13:35:01 +0000

    completed

--- 08reactRouter/src/components/Index.jsx
+++ 08reactRouter/src/components/Index.jsx
@@ -3,5 +3,8 @@ import Contact from './Contact/Contact.jsx'
 import Footer from './Footer/Footer.jsx'
 import Header from './Header/Header.jsx'
 import About from './About/About.jsx'
+import User from './User/User.jsx'
+import Github from './Github/Github.jsx'
+import { githubInfoLoader } from './Github/Github.jsx'
 
-export { Home, Contact, Footer, Header, About }
+export { Home, Contact, Footer, Header, About, User, Github,githubInfoLoader }
--- 08reactRouter/src/main.jsx
+++ 08reactRouter/src/main.jsx
@@ -2,31 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 // import Home from './components/Home/Home.jsx'
 // import About from './components/About/About.jsx'
-import {Home, Footer, About, Header, Contact} from './components/Index.jsx'
+import {Home, Footer, About, Header, Contact, User, Github,githubInfoLoader} from './components/Index.jsx'
+
+// 1st method :
+// const router = createBrowserRouter([{
+//   path : '/',
+//   element : <Layout/>,
+//   children : [
+//     {
+//       path: "",
+//       element:<Home/>
+//     },
+//     {
+//       path: "about",
+//       element:<About/>
+//     },
+//     {
+//       path:"contact",
+//       element:<Contact/>
+//     }
+//   ]
+// }])
+
+// 2nd method: more easy
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/user/:userid" element={<User/>}/>
+      <Route path="/github" element={<Github/>} loader={githubInfoLoader}/>
 
 
-const router = createBrowserRouter([{
-  path : '/',
-  element : <Layout/>,
-  children : [
-    {
-      path: "",
-      element:<Home/>
-    },
-    {
-      path: "about",
-      element:<About/>
-    },
-    {
-      path:"contact",
-      element:<Contact/>
-    }
-  ]
-}])
+    </Route>
+  )
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router ={router}/>
