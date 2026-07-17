7ea1de3aacbc81a45f9c1561f8e43ce0f7ec71d4
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-17 11:43:01 +0000

    about

--- 08reactRouter/src/main.jsx
+++ 08reactRouter/src/main.jsx
@@ -3,10 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
 
 
 const router = createBrowserRouter([{
-  path : '/'
+  path : '/',
+  element : <Layout/>,
+  children : [
+    {
+      path: "",
+      element:<Home/>
+    },
+    {}
+  ]
 }])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
